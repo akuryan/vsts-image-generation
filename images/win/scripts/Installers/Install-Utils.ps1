20b42073cf8752fb0dab4a816a057efd3fa91558
Anton Kuryan <anton.kuryan@colours.nl>
2018-10-19 21:21:05 +0300

    Added script, which will install notepad++ for now; but could be extended in future

---
+++ images/win/scripts/Installers/Install-Utils.ps1
@@ -0,0 +1,8 @@
+################################################################################
+##  File:  Initialize-VM.ps1
+##  Team:  CI-Platform
+##  Desc:  VM initialization script, machine level configuration
+################################################################################
+
+# Install webpi
+choco install notepadplusplus -y
